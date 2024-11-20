8d9aae713854b64a0248b5d177dbfd9fd185e23f
Vinicius <viniciussantanna2005@gmail.com>
2024-11-20 01:22:18 -0300

    ajustando links - página criadores

--- src/app/criadores/page.tsx
+++ src/app/criadores/page.tsx
@@ -7,11 +7,11 @@ import Pedro from "@/assets/imagem.gemeo.jpg"
 export default function Criadores(){
     return(
         <main className={styles.main}>
-            <Card imagem={Felipe} texto1="Felipe Rosa Peres" texto2="RM 000000" texto3="1TDSPX" Linkedin="Linkedin" Github="Github" href_Linkedin="" href_Github=""/>
+            <Card imagem={Felipe} texto1="Felipe Rosa Peres" texto2="RM 557636" texto3="1TDSPX" Linkedin="Linkedin" Github="Github" href_Linkedin="https://www.linkedin.com/in/felipe-peres2004" href_Github="https://github.com/Felipafaa"/>
 
             <Card imagem={Vinicius} texto1="Vinícius De Souza Sant Anna" texto2="RM 556841" texto3="1TDSPX" Linkedin="Linkedin" Github="Github" href_Linkedin="https://www.linkedin.com/in/vin%C3%ADcius-souza-sant-anna-0963ba2aa/" href_Github="https://github.com/ViniciusSantanaa"/>
 
-            <Card imagem={Pedro} texto1="Pedro Henrique De Souza" texto2="RM 555533" texto3="1TDSPX" Linkedin="Linkedin" Github="Github" href_Linkedin="" href_Github=""/>            
+            <Card imagem={Pedro} texto1="Pedro Henrique De Souza" texto2="RM 555533" texto3="1TDSPX" Linkedin="Linkedin" Github="Github" href_Linkedin="https://www.linkedin.com/in/pedro-souza-49b728142/" href_Github="https://github.com/PedroSouza1111"/>            
         </main>
     )
 }

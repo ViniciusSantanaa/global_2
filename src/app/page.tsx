b1687b7e8a1c28b0f0c39142e76b94e8d6df019a
Vinicius <viniciussantanna2005@gmail.com>
2024-11-21 04:09:24 -0300

    arrumando a página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,15 +17,20 @@ export default function Home() {
           </div>
         </section>
 
-        <div className={styles.ajustes_div_gaia}>
-          <div className={styles.div_gaia}>       
-            <h1 className={styles.h1_gaia}>O Que Gaia Representa?</h1>
+        <section className={styles.section_texto_sustentavel}>
+          <h2 className={styles.texto_sustentavel_titulo}>A importância de usarmos a energia sustentavel!</h2>
+          <p className={styles.texto_sustentavel_texto}>A energia sustentável é fundamental para o nosso futuro. Ao apostar em fontes renováveis como solar, eólica e hidrelétrica, podemos reduzir a emissão de gases poluentes e combater as mudanças climáticas. Essas fontes são inesgotáveis e não prejudicam o meio ambiente, ao contrário dos combustíveis fósseis, que são limitados e responsáveis por grande parte da poluição. Além disso, ao investir em energias limpas, estamos criando um futuro mais seguro, com mais empregos e inovação, ao mesmo tempo que garantimos maior independência energética.<br />Além dos benefícios para o planeta, a energia sustentável melhora nossa qualidade de vida. Menos poluição significa menos doenças respiratórias e cardiovasculares, e mais saúde para todos. Ao adotar energias renováveis, podemos transformar nossa economia, gerar crescimento e oferecer um futuro mais justo e próspero para as próximas gerações. Investir em energia limpa não é apenas uma escolha inteligente, é um passo essencial para um mundo mais saudável e sustentável.</p>
+        </section>
+
+        <section className={styles.ajustes_section_gaia}>
+          <div className={styles.section_gaia}>       
+            <h1 className={styles.h1_gaia}>O que Gaia representa e porque esse nome?</h1>
             <p className={styles.texto_gaia}>Gaia, na mitologia grega, é a deusa primordial da Terra, simbolizando a essência da natureza e a fonte de toda vida. Personificação do próprio planeta, Gaia surgiu do caos para dar origem ao céu , às montanhas, aos mares e a todas as formas de vida. Ela representa a energia vital que sustenta o equilíbrio do ecossistema, trazendo fertilidade, abundância e harmonia. Como protetora da natureza, Gaia nos lembra da importância de cuidar do planeta e manter a conexão entre todos os seres vivos. Sua história inspira o respeito pela Terra e a busca por um futuro mais sustentável e equilibrado.</p>
           </div>
-          <div className={styles.div_imagem_gaia}>
+          <div className={styles.section_imagem_gaia}>
             <Image src={Gaia} alt={"Imagem da Deusa Gaia"} className={styles.imagem_gaia}/>
           </div>
-        </div>
+        </section>
       </main>
       </>
   )

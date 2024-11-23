739cac6ba48b6ed65b6e9995442a690a572ad09c
Vinicius <viniciussantanna2005@gmail.com>
2024-11-22 22:31:45 -0300

    ajustes api

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -83,7 +83,7 @@ export default function Cadastro(){
                     <div className={styles.div_labels}>
                         <label htmlFor="idArrecado" className={styles.label}>Valor arrecadado (apenas números)</label>
                         <input type="text" name="valorArrecado" id="idArrecado" value={projeto.valorArrecado} 
-                        onChange={(e) => setProjeto({ ...projeto, valorMeta: Number(e.target.value) })}placeholder="Digite valor que sua empresa já arrecadou" required className={styles.input}/>
+                        onChange={(e) => setProjeto({ ...projeto, valorArrecado: Number(e.target.value) })}placeholder="Digite valor que sua empresa já arrecadou" required className={styles.input}/>
                     </div>
 
                     <div>
